6d18879ee5f26fbb3036cc619b7faaf372b518ba
fmblzf <fmblzf@example.com>
2017-02-15 18:10:23 +0800

    Scroller测试

--- view/src/main/java/com/example/view/custom/HorizontalScrollViewEx.java
+++ view/src/main/java/com/example/view/custom/HorizontalScrollViewEx.java
@@ -5,13 +5,13 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
+import android.widget.Scroller;
 
 /**
  * Created by Administrator on 2017/1/24.
  */
-public class HorizontalScrollViewEx extends HorizontalScrollView {
+public class HorizontalScrollViewEx extends ViewGroup {
 
     private static final String TAG = "HorizontalScrollViewEx";
 
@@ -19,6 +19,8 @@ public class HorizontalScrollViewEx extends HorizontalScrollView {
     private LinearLayout mLinearLayout;
     private Context mContext;
 
+
+    Scroller mScroller = null;
 
     public HorizontalScrollViewEx(Context context) {
         this(context,null);
@@ -31,13 +33,42 @@ public class HorizontalScrollViewEx extends HorizontalScrollView {
      * @param attrs
      */
     public HorizontalScrollViewEx(Context context, AttributeSet attrs) {
-        super(context, attrs,0);
+        super(context, attrs);
         Log.e(TAG,"HorizontalScrollViewEx(Context context, AttributeSet attrs)");
         this.mContext = context;
         mLinearLayout = new LinearLayout(context);
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
         mLinearLayout.setLayoutParams(params);
         this.addView(mLinearLayout);
+        mScroller = new Scroller(mContext);
+    }
+
+    /**
+     * 缓慢滑动到指定坐标位置
+     * @param destX
+     * @param destY
+     */
+    private void smoothScrollTo(int destX,int destY){
+        int scrollX = getScrollX();
+        int deltaX = destX - scrollX;
+        int scrollY = getScrollY();
+        int deltaY = destY - scrollY;
+        //1000ms滑动到指定的destX,destY位置
+        mScroller.startScroll(scrollX,scrollY,deltaX,deltaY,1000);
+        invalidate();
+    }
+
+    @Override
+    public void computeScroll() {
+        if(mScroller.computeScrollOffset()){
+            scrollTo(mScroller.getCurrX(),mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
     }
 
     /**
