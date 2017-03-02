1b83be6d5474dce32d722608fd8f3a215ac165c6
fmblzf <fmblzf@example.com>
2017-03-02 10:36:49 +0800

    完成自定义View测试

--- view/src/main/java/com/example/view/activity/CircleViewActivity.java
+++ view/src/main/java/com/example/view/activity/CircleViewActivity.java
@@ -1,16 +1,19 @@
-package com.example.view.scroller;
+package com.example.view.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.view.R;
+
 /**
- * Created by Administrator on 2017/2/6.
+ * Created by Administrator on 2017/3/2.
  */
-public class ScrollerActivity extends AppCompatActivity {
+public class CircleViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_circleview_layout);
     }
 }
